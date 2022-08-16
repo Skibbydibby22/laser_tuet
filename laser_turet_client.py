ccfa09a76e21d7aacb05a07498cb51aa784ebf8e
Skibbydibby22 <111163345+Skibbydibby22@users.noreply.github.com>
2022-08-16 17:13:49 -0500

    Update laser_turet_client.py

--- laser_turet_client.py
+++ laser_turet_client.py
@@ -1,4 +1,4 @@
-import pygame, os
+import pygame
 
 WIDTH = 900
 HEIGHT = 800
@@ -7,18 +7,17 @@ pygame.init()
 dis = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.update()
 pygame.display.set_caption('mouse_cords')
-
-game_over=False
-while not game_over:
+Clock = pygame.time.Clock()
+while True:
     mouseX, mouseY = pygame.mouse.get_pos()
     pygame.draw.line(dis , (255, 0, 0), (0, mouseY), (WIDTH, mouseY))
     pygame.draw.line(dis ,(0, 255, 0), (mouseX, 0), (mouseX, HEIGHT))
     pygame.display.update()
     dis.fill((0, 0, 0))
     print(mouseX, mouseY)
+    Clock.tick(60)
     
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
-            game_over=True
-pygame.quit()
+            pygame.quit()
 quit()
